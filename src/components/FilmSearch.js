55a306d0a4c7692c4bf87015ea0ebfc0a71bed46
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2021-02-11 13:43:51 +0200

    fix

--- src/components/FilmSearch.js
+++ src/components/FilmSearch.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Suspense, lazy } from 'react'
 import { Route, NavLink, Switch } from 'react-router-dom';
-import HomePage from '../views/HomePage/HomePage';
-import MoviesPage from '../views/MoviesPage/MoviesPage';
-import MovieDetailsPage from '../views/MovieDetailsPage/MovieDetailsPage';
+// import HomePage from '../views/HomePage/HomePage';
+// import MoviesPage from '../views/MoviesPage/MoviesPage';
+// import MovieDetailsPage from '../views/MovieDetailsPage/MovieDetailsPage';
 import NotFound from '../views/NotFound';
 import Buttons from './Buttons/Buttons';
-import Cast from '../components/Cast/Cast';
-import Reviews from '../components/Reviews/Reviews';
 
 
+const HomePage = lazy(() => import('../views/HomePage/HomePage.js' /* webpackChunkName: "home-page" */));
+const MoviesPage = lazy(()=>import('../views/MoviesPage/MoviesPage.js' /* webpackChunkName: "movies-page" */));
+const MovieDetailsPage = lazy(()=>import('../views/MovieDetailsPage/MovieDetailsPage.js' /* webpackChunkName: "details-page" */))
 
-export default class FilmSearch extends Component {
+
+export default function FilmSearch()  {
     
-
-    render() {
-        return (
-            <>
+   
+    return (
+        <>
             <Buttons/>
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route path="/movies/:movieId" component={MovieDetailsPage}/>
-                <Route path="/movies" component={MoviesPage}/>                
-                <Route path="" component={NotFound}/>
-                <Route path="/movies/:movieId/cast" component={Cast}/>
-                <Route path="/movies/:movieId/reviews" component={Reviews}/>
-            </Switch>
-            </>
-        )
-    }
+            <Suspense fallback={<h1>Загружаю...</h1>}>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route path="/movies/:movieId" component={MovieDetailsPage}/>
+                    <Route path="/movies" component={MoviesPage}/>                
+                    <Route path="" component={NotFound}/>
+                </Switch>
+            </Suspense>
+        </>
+    )
+    
 }
